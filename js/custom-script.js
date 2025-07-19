17bf0e2d57b92e801be095a1fb0f916ba31ef2fd
Akif Ullah <akifullah0317@gmail.com>
2025-07-19 14:51:18 +0500

    Update Home Page

--- js/custom-script.js
+++ js/custom-script.js
@@ -3,15 +3,41 @@ if($('.main-header').length){
 	// Hide Header on on scroll down
 	var didScroll;
 	var lastScrollTop = 0;
-	var delta = 0;
+	var delta = 5;
 	var navbarHeight = $('.main-header').outerHeight();
 
 	$(window).scroll(function(event){
 	    didScroll = true;
 	});
 
+	setInterval(function() {
+	    if (didScroll) {
+	        hasScrolled();
+	        didScroll = false;
+	    }
+	}, 250);
 
-	
+	function hasScrolled() {
+	    var st = $(this).scrollTop();
+	    
+	    // Make sure they scroll more than delta
+	    if(Math.abs(lastScrollTop - st) <= delta)
+	        return;
+	    
+	    // If they scrolled down and are past the navbar, add class .nav-up.
+	    // This is necessary so you never see what is "behind" the navbar.
+	    if (st > lastScrollTop && st > navbarHeight){
+	        // Scroll Down
+	        $('.main-header').removeClass('header-down').addClass('header-up');
+	    } else {
+	        // Scroll Up
+	        if(st + $(window).height() < $(document).height()) {
+	            $('.main-header').removeClass('header-up').addClass('header-down');
+	        }
+	    }
+	    
+	    lastScrollTop = st;
+	}
 }
 	
 
@@ -23,7 +49,7 @@ if($('.main-header').length){
 	//Hide Loading Box (Preloader)
 	function handlePreloader() {
 		if($('.loader-wrap').length){
-			$('.loader-wrap').fadeOut(500);
+			$('.loader-wrap').delay(500).fadeOut(500);
 		}
 	}
 
